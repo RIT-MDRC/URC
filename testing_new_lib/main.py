505391ac43c89d8450df83d457d4138532a48536
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2021-10-09 15:30:18 -0400

    add example resources

--- testing_new_lib/main.py
+++ testing_new_lib/main.py
@@ -1,24 +1,23 @@
-from ikpy.chain import Chain
-from ikpy.link import OriginLink, URDFLink
+import ikpy.chain
+import numpy as np
+import ikpy.utils.plot as plot_utils
 
-left_arm_chain = Chain(name='left_arm', links=[
-    OriginLink(),
-    URDFLink(
-      name="shoulder",
-      translation_vector=[-10, 0, 5],
-      orientation=[0, 1.57, 0],
-      rotation=[0, 1, 0],
-    ),
-    URDFLink(
-      name="elbow",
-      translation_vector=[25, 0, 0],
-      orientation=[0, 0, 0],
-      rotation=[0, 1, 0],
-    ),
-    URDFLink(
-      name="wrist",
-      translation_vector=[22, 0, 0],
-      orientation=[0, 0, 0],
-      rotation=[0, 1, 0],
-    )
-])
+
+my_chain = ikpy.chain.Chain.from_urdf_file("resources/poppy_ergo.URDF")
+
+# my_chain = ikpy.chain.Chain(name="my chain", links=[
+
+# ])
+
+target_position = [ 0.1, -0.2, 0.1]
+print("The angles of each joints are : ", my_chain.inverse_kinematics(target_position))
+
+real_frame = my_chain.forward_kinematics(my_chain.inverse_kinematics(target_position))
+print("Computed position vector : %s, original position vector : %s" % (real_frame[:3, 3], target_position))
+
+import matplotlib.pyplot as plt
+fig, ax = plot_utils.init_3d_figure()
+my_chain.plot(my_chain.inverse_kinematics(target_position), ax, target=target_position)
+plt.xlim(-0.1, 0.1)
+plt.ylim(-0.1, 0.1)
+plt.show()
