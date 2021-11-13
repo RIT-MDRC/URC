0595bbc7ac846f0f3fc8a95b830111f590b029aa
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2021-11-13 15:19:47 -0500

    progress

--- inverseKinematics/Arm.py
+++ inverseKinematics/Arm.py
@@ -1,6 +1,20 @@
+import numpy as np
 
+# @param k: the unit axis (kx,ky,kz) 
+# @param q: angle in radians to rotate by
+def axis_angle_rot_matrix(axis, angle):
 
-class Arm:
+    x = axis[0]
+    y = axis[1]
+    z = axis[2]
+    sin = np.sin(angle)
+    cos = np.cos(angle)
+    theta = 1 - np.cos(angle)
 
-    def __init__(self):
-        pass
+    return np.array([
+        [x * x * theta + np.cos(angle), x * y * theta - z * sin, x * z * theta + y * sin],
+        [x * y * theta + z * sin, y * y * theta + cos, y * z * theta - x * sin],
+        [x * z * theta - y * sin, y * z * theta + x * sin, z * z * theta + cos]])
+
+if __name__ == '__main__':
+  main()
