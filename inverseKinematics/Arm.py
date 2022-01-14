4082635b8e8639355e5f45a6d1eebd4d6a8649c2
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2022-01-14 18:00:56 -0500

    implement pseudo inverse jacobian IK solver

--- inverseKinematics/Arm.py
+++ inverseKinematics/Arm.py
@@ -87,11 +87,111 @@ class Arm:
             mat[2][0]
         ])
 
-    def pseudo_inverse():
-        pass
+    """
+    IK solver using the pseudo inverse jacobian method
 
-    def jacobian():
-        pass
+    @param start_angles: the starting joint angles
+    @param end_eff_translation_vector: translation vector from the last joint to the end effector
+    @param goal: x, y, z of the goal position
+    
+    @return: array containing the final joint angles
+    """
+    def pseudo_inverse(self, start_angles, end_eff_translation_vector, goal, temp):
+        angles = start_angles
+        
+        # these can be tuned as needed
+        step_size = 0.05
+        max_step = 0.2
+
+        # end: where we want the end effector to end up in the base frame
+        # current: where the end effector currently is in the global frame
+        # delta: the delta between end and current
+        end = np.array([goal[0], goal[1], goal[2]])
+        current = self.position(angles, p_i = end_eff_translation_vector)
+        delta = end - current
+
+        while np.linalg.norm(delta) > 0.01:
+            # print(f'Q[{angles}] , P[{delta}]')
+
+            # reduce by scaling factor
+            scaled_delta = delta * step_size / np.linalg.norm(delta)
+
+            jacobian = self.jacobian(angles, end_eff_translation_vector)
+            psuedo_inverse_jacobian = np.linalg.pinv(jacobian)
+            result = np.matmul(psuedo_inverse_jacobian, scaled_delta)
+
+            # move joints to the new calculated angles
+            angles = angles + np.clip(result, -1 * max_step, max_step)
+
+            # calculate the new end effector position
+            current = self.position(angles, p_i = end_eff_translation_vector)
+            delta = end - current
+
+        return angles;
+
+
+    """
+    Computes the Jacobian matrix
+
+    @param Q: n element array of current join angles (radians)
+    @param end_eff_translation_vector: translation vector last joint to end effector relative to the last joint
+
+    @return: 3xN 2D Jacobian matrix
+    """
+    def jacobian(self, Q, end_eff_translation_vector=[0,0,0]):
+        end_effector_global_pos = self.position(Q, -1, end_eff_translation_vector)
+        jacobian = []
+
+        joint_offset = end_effector_global_pos - self.position(Q,index=0)
+
+        # Axes
+        kx = self.k[0][0]
+        ky = self.k[0][1]
+        kz = self.k[0][2]
+        k = np.array([kx, ky, kz])
+
+        px = joint_offset[0]
+        py = joint_offset[1]
+        pz = joint_offset[2]
+        joint_offset = np.array([px, py, pz])
+
+        this_jacobian = np.cross(k, joint_offset)
+
+        # Convert to a 2D matrix
+        j0 = this_jacobian[0]
+        j1 = this_jacobian[1]
+        j2 = this_jacobian[2]
+        this_jacobian = np.array([[j0],
+                                [j1],
+                                [j2]])
+        jacobian = this_jacobian
+
+        for i in range(1, self.n):
+            p_eff_minus_this_p = end_effector_global_pos - self.position(Q,index=i)
+
+            # Axes
+            kx = self.k[i][0]
+            ky = self.k[i][1]
+            kz = self.k[i][2]
+            k = np.array([kx, ky, kz])
+
+            # FIXME lmao I didn't even notice this
+            px = joint_offset[0]
+            py = joint_offset[1]
+            pz = joint_offset[2]
+            joint_offset = np.array([px, py, pz])
+
+            this_jacobian = np.cross(k, joint_offset)
+
+            # Convert to a 2D matrix
+            j0 = this_jacobian[0]
+            j1 = this_jacobian[1]
+            j2 = this_jacobian[2]
+            this_jacobian = np.array([[j0],
+                                    [j1],
+                                    [j2]])
+            jacobian = np.concatenate((jacobian, this_jacobian), axis=1) # side by side
+        return jacobian;
 
 def main():
     pass
