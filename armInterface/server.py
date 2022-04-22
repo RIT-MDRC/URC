1342ece3e9db349bc76aa28ea8e2559c5127ca5e
williamwright5191 <williamwright5191@gmail.com>
2022-04-23 00:11:52 +0100

    serial stuff

--- armInterface/server.py
+++ armInterface/server.py
@@ -1,6 +1,8 @@
 import numpy as np
 import socket
 import json
+import serial
+import time
 
 # move this to a different place
 class Vector:
@@ -10,7 +12,7 @@ class Vector:
         self.z = z
 
 class Server:
-    def __init__(self, listen_socket = socket.socket(), port = 8000, address = 'localhost'):
+    def __init__(self, listen_socket = socket.socket(), port = 8000, address = 'urc.student.rit.edu'):
         self.terminated     = False
         self.listen_socket  = listen_socket
         self.port           = port
@@ -32,28 +34,40 @@ class Server:
     def execute_message(self, message):
         command = message.get('command')
         info    = message.get('info')
+        send_to_board(command)
+        
+"""
+        if command == 'terminate':
+            print('Terminating...')
+            self.terminated = True
+        elif command == 'x_offset':
+            print(f'Move {info} x')
+        elif command == 'y_offset':
+            print(f'Move {info} y')
+        elif command == 'z_offset':
+            print(f'Move {info} z')
+        elif command == 'rotate_base':
+            degrees = info['degrees']
+            direction = info['direction']
+            print(f'rotate base {degrees} degrees {direction}')
+        elif command == 'rotate_end_effector':
+            degrees = info['degrees']
+            direction = info['direction']
+            print(f'rotate end effector {degrees} degrees {direction}')
 
-        match command:
-            case 'terminate':
-                print('Terminating...')
-                self.terminated = True
-            case 'x_offset':
-                print(f'Move {info} x')
-            case 'y_offset':
-                print(f'Move {info} y')
-            case 'z_offset':
-                print(f'Move {info} z')
-            case 'rotate_base':
-                degrees = info['degrees']
-                direction = info['direction']
-                print(f'rotate base {degrees} degrees {direction}')
-            case 'rotate_end_effector':
-                degrees = info['degrees']
-                direction = info['direction']
-                print(f'rotate end effector {degrees} degrees {direction}')
+        else:
+            print('Invalid json  :(')
+"""
 
-            case _:
-                print('Invalid json  :(')
+def send_to_board(message):
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+    ser.reset_input_buffer()
+    print(message)
+#    while True:
+    ser.write(bytes(message, 'utf-8'))
+#        line = ser.readline().decode('utf-8').rstrip()
+#        print(line)
+#        time.sleep(1)
 
 def main():
     server = Server()
